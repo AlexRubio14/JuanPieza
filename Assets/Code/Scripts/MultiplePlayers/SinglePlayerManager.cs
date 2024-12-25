842c5149b56a09c78cbfd6c14d3672ffb5f4c2aa
Crami <crami.andrada@gmail.com>
2024-12-25 03:13:07 +0100

    Multiple player Assets imported from Prototype

---
+++ Assets/Code/Scripts/MultiplePlayers/SinglePlayerManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SinglePlayerManager : MonoBehaviour
+{
+    /* Este Script tiene que hacer lo siguiente
+     * Tiene que ser don't destroy on load
+     * Tiene que contener el player input
+     * Tiene que contener variables de player como por ejemplo los cosmeticos
+     * Al entrar estar en una escena 
+     */
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
