e567cd93add7ff5e06d871b7852e9e51ad2b577f
PauGi <pau.giro@enti.cat>
2025-01-06 18:00:54 +0100

    Double Cannon FInished

--- Assets/Code/Scripts/Objects/Weapons/Cannon/CannonDouble.cs
+++ Assets/Code/Scripts/Objects/Weapons/Cannon/CannonDouble.cs
@@ -1,7 +1,20 @@
+using UnityEngine;
+
 public class CannonDouble : Weapon
 {
+    [Space, Header("CannonDouble"), SerializeField]
+    private GameObject bulletPrefab;
+    [SerializeField] private Transform bulletSpawnPosRight;
+    [SerializeField] private Transform bulletSpawnPosLeft;
+
     protected override void Shoot()
     {
-        throw new System.NotImplementedException();
+        GameObject newBulletRight = Instantiate(bulletPrefab, bulletSpawnPosRight.transform.position, Quaternion.identity);
+        newBulletRight.GetComponent<Rigidbody>().AddForce(bulletSpawnPosRight.forward * bulletForce, ForceMode.Impulse);
+
+        GameObject newBulletLeft = Instantiate(bulletPrefab, bulletSpawnPosLeft.transform.position, Quaternion.identity);
+        newBulletLeft.GetComponent<Rigidbody>().AddForce(bulletSpawnPosLeft.forward * bulletForce, ForceMode.Impulse);
+
+        hasAmmo = false;
     }
 }
