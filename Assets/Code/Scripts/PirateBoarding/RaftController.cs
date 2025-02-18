d751ebcd5cb8749d9410241fd3a08ea20f0b3266
AlexRP <alexrubioplana14@gmail.com>
2025-02-19 00:52:27 +0100

    Rafts system prototype

--- Assets/Code/Scripts/PirateBoarding/RaftController.cs
+++ Assets/Code/Scripts/PirateBoarding/RaftController.cs
@@ -10,18 +10,49 @@ public class RaftController : MonoBehaviour
 
     [SerializeField] private List<controllerPirateBoarding> pirates;
 
+    [SerializeField] private Rigidbody rb;
+
+    private float direction = 1;
+    [SerializeField] private float speed;
+
+    public Vector3 destinyPos { get; private set; }
+    public Vector3 startingPos { get; private set; }
+
+    public enum RaftState { WAITING, MOVING_FRONT, MOVING_BACK, BOARDING }
+    public RaftState currentState { get; private set; }
     public bool isBoarding { get; private set; } = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (currentState)
+        {
+            case RaftState.WAITING:
+                break;
+            case RaftState.MOVING_FRONT:
+
+                if(transform.position.z >= destinyPos.z)
+                {
+                    ChangeState(RaftState.BOARDING);
+                }
+                break;
+            case RaftState.MOVING_BACK:
+
+                if (transform.position.z <= startingPos.z)
+                {
+                    ChangeState(RaftState.WAITING);
+                    ResetRaft();
+                    
+                }
+                break;
+            case RaftState.BOARDING:
+
+                direction = -1;
+                break;
+            default:
+                break;
+        }
     }
 
     public void SetUpRaft()
@@ -31,11 +62,32 @@ public class RaftController : MonoBehaviour
 
         int enemiesToSpawn = CalculateRandomEnemiesToSpawn();
 
-        InstantiatePirates(enemiesToSpawn);
+        SetPiratesInRaft(enemiesToSpawn);
 
         isBoarding = true;
 
-        StartBoarding();
+        List<Transform> raftStartPointsRef = managerPirateBoarding.Instance.raftStartPoints;
+        int randomIndex = Random.Range(0, raftStartPointsRef.Count);
+
+        transform.position = raftStartPointsRef[randomIndex].position;
+
+        ChangeState(RaftState.MOVING_FRONT);
+        startingPos = transform.position;
+    }
+
+    private void ResetRaft()
+    {
+        transform.position = transform.parent.position;
+
+        foreach(controllerPirateBoarding controller in pirates)
+        {
+            controller.transform.SetParent(managerPirateBoarding.Instance.piratesHolder, true);
+            controller.transform.position = managerPirateBoarding.Instance.piratesHolder.transform.position;
+        }
+
+        pirates.Clear();
+
+        isBoarding = false;
     }
 
     private int CalculateRandomEnemiesToSpawn()
@@ -61,10 +113,11 @@ public class RaftController : MonoBehaviour
         return enemiesToSpawn;
     }
 
-    private void InstantiatePirates(int _enemiesToSpawn)
+    private void SetPiratesInRaft(int _enemiesToSpawn)
     {
         for (int i = 0; i < _enemiesToSpawn; i++)
         {
+            //If there is no available pirates Instantiate more
             if (managerPirateBoarding.Instance.piratesPool.Count == 0)
             {
                 managerPirateBoarding.Instance.InstantiatePirates();
@@ -78,8 +131,66 @@ public class RaftController : MonoBehaviour
         }
     }
 
+
+    public void ChangeState(RaftState newState)
+    {
+        switch (currentState)
+        {
+            case RaftState.WAITING:
+                break;
+            case RaftState.MOVING_FRONT:
+
+                break;
+            case RaftState.MOVING_BACK:
+                break;
+            case RaftState.BOARDING:
+                break;
+            default:
+                break;
+        }
+
+        switch (newState)
+        {
+            case RaftState.WAITING:
+                rb.linearVelocity = Vector3.zero;
+                break;
+            case RaftState.MOVING_FRONT:
+                direction = 1;
+                rb.linearVelocity = transform.forward * speed * direction;
+                break;
+            case RaftState.MOVING_BACK:
+                direction = -1;
+                rb.linearVelocity = transform.forward * speed * direction;
+                break;
+            case RaftState.BOARDING:
+                rb.linearVelocity = Vector3.zero;
+                Invoke("StartMovingBack", 2.0f);
+                break;
+            default:
+                break;
+        }
+
+        currentState = newState;
+    }
+
+    private void StartMovingFront()
+    {
+        ChangeState(RaftState.MOVING_FRONT);
+    }
+
+    private void StartMovingBack()
+    {
+        ChangeState(RaftState.MOVING_BACK);
+    }
+
     private void StartBoarding()
     {
-        Debug.Log("Boardeo");
+        ChangeState(RaftState.BOARDING);
     }
+
+    public void SetDestinyPos(Vector3 pos)
+    {
+        destinyPos = pos;
+    }
+
 }
--- Assets/Code/Scripts/PirateBoarding/managerPirateBoarding.cs
+++ Assets/Code/Scripts/PirateBoarding/managerPirateBoarding.cs
@@ -1,19 +1,30 @@
 using NUnit.Framework;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class managerPirateBoarding : MonoBehaviour
 {
     public static managerPirateBoarding Instance { get; private set; }
 
-    [SerializeField] private List<RaftController> rafts;
+    [Header("Boarding Pirates")]
     [SerializeField] private GameObject boardingEnemy;
+    [SerializeField] public Transform piratesHolder;
     [SerializeField] private int piratesToSpawn;
-    [SerializeField] private Transform piratesHolder;
-
     [SerializeField] public List<controllerPirateBoarding> piratesPool { get; private set; } = new List<controllerPirateBoarding>();
 
-    private List<Transform> raftsStartPos;
+    [Header("Rafts")]
+    [SerializeField] private GameObject raft;
+    [SerializeField] public Transform raftsHolder;
+    [SerializeField] private int raftsToSpawn;
+    [SerializeField] public List<RaftController> raftsPool { get; private set; } = new List<RaftController> { };
+
+
+    [Header("Raft Start Pos")]
+    [SerializeField] private List<Transform> raftsStartPos = new List<Transform>();
+    public List<Transform> raftStartPoints = new List<Transform>();
+    [SerializeField] private float zOffset;
+    [SerializeField] private Transform startPosHolder;
 
     private void Awake()
     {
@@ -30,20 +41,53 @@ public class managerPirateBoarding : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        InstantiatePirates();
+        InstantiateContent();
+        CalculateRaftSpawnPoints();
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyUp(KeyCode.H)) {
-            rafts[0].SetUpRaft();
+            raftsPool[0].SetUpRaft();
         }
     }
 
+    // This method should be called everytime a new stage starts 
+    // TODO: Call the method CalculateRaftSpawnPoints when a stage is started
     private void CalculateRaftSpawnPoints()
     {
+        int index = ShipsManager.instance.enemiesShips.Count;
 
+        Vector3 playerShipPos = ShipsManager.instance.playerShip.transform.position;
+
+        List<Ship> _enemiesShips = ShipsManager.instance.enemiesShips;
+
+        for(int i = 0; i < index; i++)
+        {
+            Vector3 midPosBetweenShips = (playerShipPos + _enemiesShips[i].transform.position) / 2;
+
+            raftsStartPos[i].transform.position = new Vector3(midPosBetweenShips.x, midPosBetweenShips.y, midPosBetweenShips.z - zOffset);
+            raftStartPoints.Add(raftsStartPos[i]);
+
+            raftsPool[i].SetDestinyPos(midPosBetweenShips);
+        }
+    }
+
+    // Create the Pools of boarding Content in the manager, so it persists perhaps the SCENE IS CHANGED
+    // TODO: Call the method InstantiateContent when a run is started
+    public void InstantiateContent()
+    {
+        InstantiateRafts();
+        InstantiatePirates();
+    }
+
+    // This method erase the pools created in the manager, BE SURE of Instantiate the content again when a run is started again
+    // TODO: Call the method DeleteContent when a run is over
+    public void DeleteContent()
+    {
+        DeletePiratePool();
+        DeleteRaftPool();
     }
 
     public void InstantiatePirates()
@@ -55,10 +99,55 @@ public class managerPirateBoarding : MonoBehaviour
             if (pirate.TryGetComponent(out controllerPirateBoarding controller))
             {
                 piratesPool.Add(controller);
-                Debug.Log("Pirates Added");
-                Debug.Log(i);
                 controller.gameObject.transform.SetParent(piratesHolder, true);
             }
         }
     }
+
+    public void InstantiateRafts()
+    {
+        for(int i = 0; i < raftsToSpawn; i++)
+        {
+            GameObject tempRaft = Instantiate(raft, raftsHolder.position, raft.transform.rotation);
+
+            if(tempRaft.TryGetComponent(out RaftController controller))
+            {
+                raftsPool.Add(controller);
+                controller.gameObject.transform.SetParent(raftsHolder, true);
+            }
+        }
+    }
+
+    public void DeletePiratePool()
+    {
+        foreach(controllerPirateBoarding controller in piratesPool)
+        {
+            if(controller != null)
+            {
+                Destroy(controller.gameObject);
+            }
+        }
+    }
+
+    public void DeleteRaftPool()
+    {
+        foreach(RaftController raft in raftsPool)
+        {
+            if(raft != null)
+            {
+                Destroy(raft.gameObject);
+            }
+        }
+    }
+
+    //This method should be called when a runs is over
+    // TODO: call the method when a run is over
+    void ResetRaftStartPos()
+    {
+        foreach(Transform gameObject in raftsStartPos)
+        {
+            gameObject.transform.position = startPosHolder.position;
+        }
+    }
+
 }
