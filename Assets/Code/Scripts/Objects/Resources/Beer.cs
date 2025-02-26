35f5f17b80fa0f94fe6ad2be029e0a295203df8e
Crami Enti <marc.andrada@enti.cat>
2025-02-26 11:08:00 +0100

    Some Ui interact Icons and bug fix

--- Assets/Code/Scripts/Objects/Resources/Beer.cs
+++ Assets/Code/Scripts/Objects/Resources/Beer.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class Beer : Resource
 {
+
+    public override void Interact(ObjectHolder _objectHolder)
+    {
+        base.Interact(_objectHolder);
+        _objectHolder.hintController.UpdateActionType(ShowNeededInputHint(_objectHolder));
+    }
     public override void Use(ObjectHolder _objectHolder)
     {
         //playear animacion player
@@ -9,5 +15,26 @@ public class Beer : Resource
         _objectHolder.RemoveItemFromHand();
         _objectHolder.GetComponentInParent<PlayerController>().animator.SetBool("Pick", false);
         Destroy(gameObject);
+    }
+
+    public override HintController.Hint[] ShowNeededInputHint(ObjectHolder _objectHolder)
+    {
+        if (!_objectHolder.GetHasObjectPicked())
+            return new HintController.Hint[]
+            {
+                new HintController.Hint(HintController.ActionType.INTERACT, "grab"),
+                new HintController.Hint(HintController.ActionType.CANT_USE, "")
+            };
+        else if (_objectHolder.GetHandInteractableObject() == this)
+            return new HintController.Hint[]
+            {
+                new HintController.Hint(HintController.ActionType.INTERACT, "drop"),
+                new HintController.Hint(HintController.ActionType.USE, "drink")
+            };
+
+        return new HintController.Hint[]
+        {
+            new HintController.Hint(HintController.ActionType.NONE, "")
+        };
     }
 }
