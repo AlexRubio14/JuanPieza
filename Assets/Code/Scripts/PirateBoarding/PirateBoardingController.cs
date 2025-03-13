73b12cb2d5d387785543fe2c2d3e04b7560b4a81
Alex Rubio <alejandro.rubio@enti.cat>
2025-03-13 23:15:42 +0100

    Pirates picks randomly the pos to jump

--- Assets/Code/Scripts/PirateBoarding/PirateBoardingController.cs
+++ Assets/Code/Scripts/PirateBoarding/PirateBoardingController.cs
@@ -60,6 +60,7 @@ public class PirateBoardingController : MonoBehaviour
             case PirateState.WAITING:
                 break;
             case PirateState.PARABOLA:
+                transform.forward = transform.position - jumpStartPos;
                 JumpIntoPlayerShip();
                 break;
             case PirateState.CHASING:
@@ -162,19 +163,13 @@ public class PirateBoardingController : MonoBehaviour
     {
         List<Transform> boardingPoints = ShipsManager.instance.playerShip.boardingPoints;
 
-        Vector3 closestPoint = Vector3.zero;
-        float tempDistance = 100;
+        SortBoardingPoints(boardingPoints); 
 
-        foreach (Transform boardingPoint in boardingPoints)
-        {
-            Vector3 disFromPirateToPoint = boardingPoint.position - transform.position;
+        int randomIndex = Random.Range(0, Mathf.Min(3, boardingPoints.Count)); // Pick a random value between the first 3 positions
 
-            if(disFromPirateToPoint.magnitude <= tempDistance)
-            {
-                closestPoint = boardingPoint.position;
-                tempDistance = disFromPirateToPoint.magnitude;
-            }
-        }
+        Vector3 closestPoint = boardingPoints[randomIndex].position;
+
+        //Uses raycast to adjust the position to jump, to the raft collision
 
         Vector3 raycastPoint = new Vector3(closestPoint.x, closestPoint.y + raycastDis, closestPoint.z);
 
@@ -184,6 +179,18 @@ public class PirateBoardingController : MonoBehaviour
         {
             posToJump = raycastHit.point;
         }
+    }
+
+    /// <summary>
+    /// Sorts the given list of Transforms by their distance to this object's position, in ascending order.
+    /// The closest Transform will be at index 0.
+    /// </summary>
+    public void SortBoardingPoints(List<Transform> boardingPoints)
+    {
+        boardingPoints.Sort((a, b) =>
+            Vector3.Distance(transform.position, a.position).CompareTo(
+            Vector3.Distance(transform.position, b.position))
+        );
     }
 
     private void CalculateTarget()
