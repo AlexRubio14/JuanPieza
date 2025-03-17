3f47cf4f61bea34186a0dcfa40c39a7b61ebdb73
Crami <crami.andrada@gmail.com>
2025-03-17 22:27:46 +0100

    Butons and hints fixed

--- Assets/Code/Scripts/Combat/QuestBoard/AcceptButton.cs
+++ Assets/Code/Scripts/Combat/QuestBoard/AcceptButton.cs
@@ -4,17 +4,23 @@ using UnityEngine.UI;
 public class AcceptButton : MonoBehaviour
 {
     private Button acceptButton;
+    private ImageFloatEffect floatEffect;
 
     private void Awake()
     {
         acceptButton = GetComponent<Button>();
 
         acceptButton.onClick.AddListener(OnButtonClick);
+
+        floatEffect = GetComponent<ImageFloatEffect>();
     }
 
     private void OnEnable()
     {
         acceptButton.Select();
+
+        if (floatEffect)
+            floatEffect.canFloat = true;
     }
 
     void OnButtonClick()
--- Assets/Code/Scripts/Combat/QuestBoard/ExitPanel.cs
+++ Assets/Code/Scripts/Combat/QuestBoard/ExitPanel.cs
@@ -7,13 +7,21 @@ public class ExitPanel : MonoBehaviour
     [SerializeField] private GameObject informationCanvas;
     [SerializeField] private QuestBoard questBoard;
 
+    private ImageFloatEffect floatEffect;
     private void Awake()
     {
         exitButton = GetComponent<Button>();
 
         exitButton.onClick.AddListener(OnButtonClick);
+
+        floatEffect = GetComponent<ImageFloatEffect>();
     }
 
+    private void OnEnable()
+    {
+        if (floatEffect)
+            floatEffect.canFloat = false;
+    }
     void OnButtonClick()
     {
         informationCanvas.SetActive(false);
