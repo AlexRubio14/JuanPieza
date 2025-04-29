8898a59dac6224b7ad3baab02de8074cf185439c
Daniel Martinez <daniel.martinez.liu@enti.cat>
2025-04-29 21:24:05 +0200

    Credits added

--- Assets/Code/Scripts/Menu/AudioMenuController.cs
+++ Assets/Code/Scripts/Menu/AudioMenuController.cs
@@ -11,15 +11,7 @@ public class AudioMenuController : MonoBehaviour
     private Slider musicSlider;
     [SerializeField]
     private Slider sfxSlider;
-
-    [Space, SerializeField]
-    private GameObject menuObject;
-
-    [SerializeField]
-    private Button settingsStartSelectedButton;
-    [SerializeField]
-    private Button menuSettingsButton;
-
+    
     private void Start()
     {
         UpdateMusicVolume();
@@ -35,20 +27,5 @@ public class AudioMenuController : MonoBehaviour
     {
         float volume = (sfxSlider.value / 10) + 0.0001f;
         audioMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
-    }
-
-
-    public void OpenSettings()
-    {
-        menuObject.SetActive(false);
-        gameObject.SetActive(true);
-        settingsStartSelectedButton.Select();
-    }
-
-    public void CloseSettings()
-    {
-        menuObject.SetActive(true);
-        gameObject.SetActive(false);
-        menuSettingsButton.Select();
     }
 }
