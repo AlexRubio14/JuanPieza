7755652ee3ccfc8880b7914ebd5b637635ddbf75
Crami <crami.andrada@gmail.com>
2025-05-03 16:22:13 +0200

    Radio break effect done

--- Assets/Code/Scripts/Objects/Decoration/Radio.cs
+++ Assets/Code/Scripts/Objects/Decoration/Radio.cs
@@ -1,13 +1,18 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Radio : Resource, ICatapultAmmo
 {
     [Space, Header("Radio"), SerializeField] private List<AudioClip> musicsList;
     private List<float> musicsPlayed = new List<float>();
+    [field: SerializeField]
+    public float timeToBreakRadio {  get; private set; }
     [SerializeField]
-    private AudioClip radioClip;
-
+    private AudioClip toggleRadioClip;
+    [field: SerializeField]
+    public AudioClip breakRadioClip { get; private set; }
     protected override void Awake()
     {
         base.Awake();
@@ -22,7 +27,7 @@ public class Radio : Resource, ICatapultAmmo
     public override void Use(ObjectHolder _objectHolder) 
     {
         if (AudioManager.instance.radioAs.isPlaying)
-            StopPlaying();
+            StopPlaying(toggleRadioClip);
         else
             Play();
     }
@@ -35,7 +40,6 @@ public class Radio : Resource, ICatapultAmmo
     private void Play()
     {
         int randomMusic;
-
         do
         {
             randomMusic = Random.Range(0, musicsList.Count);
@@ -49,18 +53,29 @@ public class Radio : Resource, ICatapultAmmo
 
         AudioManager.instance.musicAs.Pause();
         AudioManager.instance.PlayLoopSound(AudioManager.instance.radioAs, musicsList[randomMusic], "Radio", 1f, 1f, 1f);
-        AudioManager.instance.Play2dOneShotSound(radioClip, "Objects", 0.5f, 0.6f, 0.8f);
+        AudioManager.instance.Play2dOneShotSound(toggleRadioClip, "Objects", 0.5f, 0.6f, 0.8f);
     }
 
-    private void StopPlaying()
+    private static void StopPlaying(AudioClip _buttonClip)
     {
         AudioManager.instance.musicAs.UnPause();
         AudioManager.instance.radioAs.clip = null;
         AudioManager.instance.radioAs.Stop();
-        AudioManager.instance.Play2dOneShotSound(radioClip, "Objects", 0.5f, 1.1f, 1.3f);
-
+        if(_buttonClip)
+            AudioManager.instance.Play2dOneShotSound(_buttonClip, "Objects", 0.5f, 1.1f, 1.3f);
     }
-    
+    public static void BreakRadio(AudioClip _breakClip)
+    {
+        AudioManager.instance.Play2dOneShotSound(_breakClip, "Objects", 1, 0.95f, 1.05f);
+        StopPlaying(null);
+    }
+
+    public static IEnumerator PlayMainMusic()
+    {
+        AudioManager.instance.musicAs.Pause();
+        yield return new WaitForSeconds(1);
+        AudioManager.instance.musicAs.UnPause();
+    }
     public void RadioFallAtWater()
     {
         AudioManager.instance.ApplyUnderwaterFilter(AudioManager.instance.radioAs);
